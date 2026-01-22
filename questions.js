b9fe8586da0ef063a515359f479ffb989aae9f21
corby88 <luca.corbacella@gmail.com>
2026-01-23 00:10:12 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    text: "SQL è un linguaggio che:",
+    answers: [
+      { text: "offre solo DDL", correct: false },
+      { text: "offre solo DML", correct: false },
+      { text: "offre sia DDL sia DML", correct: true },
+      { text: "non gestisce dati", correct: false }
+    ]
+  },
+  {
+    text: "I DBMS gestiscono dati:",
+    answers: [
+      { text: "piccoli e privati", correct: false },
+      { text: "grandi, persistenti e condivisi", correct: true },
+      { text: "temporanei", correct: false },
+      { text: "non strutturati", correct: false }
+    ]
+  }
+];
